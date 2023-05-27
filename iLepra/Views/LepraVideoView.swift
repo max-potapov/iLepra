224441282b426edf9d90c56c8815072a3536afcc
Maxim V. Potapov <vopatop.skam@gmail.com>
2023-05-27 18:36:09 +0200

    enable video auto playback

--- iLepra/Views/LepraVideoView.swift
+++ iLepra/Views/LepraVideoView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct LepraVideoView: View {
     let url: URL
+    @State private var globalFrame: CGRect = .zero
+    private let height: CGFloat = 400
 
     var body: some View {
         let player: AVPlayer = .init(url: url)
@@ -18,8 +20,36 @@ struct LepraVideoView: View {
             .onDisappear {
                 player.pause()
             }
-            .frame(height: 400)
+            .frame(height: height)
             .clipped()
+        #if os(iOS)
+            .overlay(
+                GeometryReader { proxy in
+                    Color.clear.preference(key: RectPreferenceKey.self, value: proxy.frame(in: .global))
+                }
+            )
+            .onPreferenceChange(RectPreferenceKey.self) { value in
+                globalFrame = value
+
+                let frameCenter = value.midY
+                let screenCenter = UIScreen.main.bounds.size.height / 2
+
+                if abs(frameCenter - screenCenter) < height / 1.3 {
+                    player.allowsExternalPlayback = true
+                    player.isMuted = true
+                    player.play()
+                } else {
+                    player.pause()
+                }
+            }
+        #endif
+    }
+}
+
+private struct RectPreferenceKey: PreferenceKey {
+    static var defaultValue: CGRect = .zero
+    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
+        value = nextValue()
     }
 }
 
